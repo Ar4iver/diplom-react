54e19b82fa0c0f1a4c904f4d05a71fb115b3abdc
Igor <qwertyhock@gmail.com>
2023-11-28 09:09:04 +0300

    fix

--- build/331.a864562d5ffe0a30096c.js
+++ build/331.a864562d5ffe0a30096c.js
@@ -1,1 +1,13 @@
-"use strict";(self.webpackChunkdiplom_react=self.webpackChunkdiplom_react||[]).push([[331],{331:(e,c,r)=>{r.r(c),r.d(c,{default:()=>n});var t=r(893);const n=function(){return(0,t.jsx)("div",{children:"главная страница"})}}}]);
+'use strict'
+;(self.webpackChunkdiplom_react = self.webpackChunkdiplom_react || []).push([
+	[331],
+	{
+		331: (e, c, r) => {
+			r.r(c), r.d(c, { default: () => n })
+			var t = r(893)
+			const n = function () {
+				return (0, t.jsx)('div', { children: 'главная страница' })
+			}
+		},
+	},
+])
