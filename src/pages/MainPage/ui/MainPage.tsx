8a93f955e9d0ffdbef148ff9aa6173081c4518bb
ar4iver <qwertyhock@gmail.com>
2024-01-12 12:56:13 +0300

    refact component timer

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,114 +1,14 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 import cls from './MainPage.module.scss'
-import AddTimeBigAction from 'shared/assets/icons/btn__action__timer__add_big.svg'
-import { Button } from 'shared/ui/Button/Button'
 import { formatTime } from 'shared/lib/helpers/formatTime'
-import { classNames } from 'shared/lib/classNames/classNames'
+import { TodoList } from 'entities/Task'
 import { useSelector } from 'react-redux'
 import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema'
-import { TodoList, taskActions } from 'entities/Task'
 import { TodoForm } from 'features/taskForm'
-import { useAppDispatch } from 'app/providers/StoreProvider/config/store'
-
-const totalSeconds = 100
-const timeTodos = 200
+import { TaskTimer } from 'features/taskTimer'
 
 const MainPage = () => {
-
-
-	const dispatch = useAppDispatch()
 	const tasks = useSelector((state: StateSchema) => state.tasks)
-	const activeTask = useSelector((state: StateSchema) => state.tasks.activeTask);
-	const activeTaskTime = useSelector((state: StateSchema) => state.tasks.activeTask?.taskTime);
-	const activeTaskCountPomidor = useSelector((state: StateSchema) => state.tasks.activeTask?.countPomidor);
-
-	const [startTimerApp, setStartTimerApp] = useState(false)
-
-	const [indexTask, setIndexTask] = useState(0)
-	const [countSession, setCountSession] = useState(1)
-
-	const [timebreakLong, setTimeBreakLong] = useState(10)
-	const [timebreakShort, setTimeBreakShort] = useState(5)
-	const [isBreak, setIsBreak] = useState(false)
-
-
-	const startTimer = () => {
-		setStartTimerApp(true)
-	}
-
-
-	//Выполнить то, если isBreak изменился.
-	useEffect(() => {
-		if (indexTask < tasks.tasks.length) {
-			dispatch(taskActions.setActiveTask(tasks.tasks[indexTask].id))
-			if (!isBreak) {
-				if (activeTask) {
-					const mainIntervalId = setInterval(() => {
-						dispatch((dispatch, getState) => {
-							const { activeTask } = getState().tasks
-							console.log(activeTask!.taskTime)
-							console.log(activeTask!.countPomidor)
-							if (activeTask) {
-								if (activeTask.taskTime > 0) {
-									dispatch(taskActions.tickTimerTask())
-								} else {
-									if (activeTask.taskTime === 0 && activeTask.countPomidor === 1) {
-										console.log('старт следующей задачи')
-										clearInterval(mainIntervalId);
-										setIndexTask(indexTask + 1)
-									} else if (activeTask.taskTime === 0 && activeTask.countPomidor != 1) {
-										console.log('старт следующей помидорки дял задачи')
-										dispatch(taskActions.tickPomidorTask(activeTask.id))
-										clearInterval(mainIntervalId);
-									}
-									setIsBreak(true);
-									setCountSession(prevCount => prevCount + 1);
-									startTimer()
-								}
-							}
-						})
-					}, 1000)
-				}
-			} else {
-				const isbreakId = setInterval(() => {
-					if (countSession === 4) {
-						console.log('начался длинный перерыв')
-						setTimeBreakLong(prev => {
-							if (prev === 0) {
-								clearInterval(isbreakId)
-								setTimeBreakLong(10)
-								setIsBreak(false)
-							}
-							return prev - 1
-						})
-					} else {
-						console.log('начался короткий перерыв')
-						setTimeBreakShort(prev => {
-							if (prev === 0) {
-								clearInterval(isbreakId)
-								setTimeBreakShort(5)
-								setIsBreak(false)
-							}
-							return prev - 1
-						})
-
-					}
-				}, 1000)
-				startTimer()
-			}
-		}
-	}, [isBreak, startTimerApp])
-
-
-	///Что будет после первого запуска приложения
-	useEffect(() => {
-		if (tasks.tasks.length != 0 && !activeTask) {
-			console.log('вставка активной задачи c useEffect')
-			dispatch(taskActions.setActiveTask(tasks.tasks[0].id))
-		}
-	}, [])
-
-
 	return (
 		<section className={cls.section__app}>
 			<div className={cls.leftContent}>
@@ -135,49 +35,12 @@ const MainPage = () => {
 						<TodoList tasks={tasks} />
 					</div>
 					<span>
-						{timeTodos ? formatTime(totalSeconds, 'sumTime') : ''}
+						суммарное время всех задач
 					</span>
 				</div>
 			</div>
 			<div className={cls.rightContent}>
-				<div className={classNames(cls.timerHeader, {})}>
-					<div>{activeTask?.taskSummary}</div>
-					<div>Помидор {activeTask?.countPomidor}</div>
-				</div>
-				<div className={cls.timerBlock}>
-					<span className={cls.timerContent}>
-						<span className={classNames(cls.timer, {})}>
-							{activeTask?.taskTime
-								? formatTime(activeTask?.taskTime, 'timer')
-								: '00:00'}
-						</span>
-						<span className={cls.iconBtnActionAddTime}>
-							<AddTimeBigAction />
-						</span>
-					</span>
-				</div>
-				<div className={cls.taskTimer}>
-					Задача (порядковый номер в списке) - (название задачи)
-				</div>
-				<div className={cls.btnTimerAction}>
-					<>
-						<Button
-							className={classNames(cls.timerButtonStart, {})}
-							onClick={() => startTimer()}
-						>
-							Старт
-						</Button>
-						<Button
-							className={classNames(
-								cls.timerButtonStopUnactive,
-								{}
-							)}
-						// onClick={stopTimer}
-						>
-							Стоп
-						</Button>
-					</>
-				</div>
+				<TaskTimer />
 			</div>
 		</section>
 	)
